2200128885b133b7023b278f6306903ba279b94b
miki018 <m.miki@mail.com>
2020-11-01 10:31:46 +0100

    a11

--- miki/test6/src/main/java/test6/App.java
+++ miki/test6/src/main/java/test6/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("miki Hello World!" );
         System.out.println(Nzd.gcd3(24, 36));
         System.out.println(Nzd.nzd(24, 36));
     }
